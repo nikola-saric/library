ed16188c781b33982a0ef2b880669451bd64116f
Nikola Saric <azzotha@gmail.com>
2016-09-24 13:05:31 +0200

    Test

--- src/com/nikolasaric/Library.java
+++ src/com/nikolasaric/Library.java
@@ -1,8 +1,21 @@
 package com.nikolasaric;
 
+//Collects informations about books and prints them out.
 public class Library {
 
+	
 	public static void main(String[] args) {
-		System.out.print("Cao Nidzo!");
+		Books newBooks = new Books();
+
+		
+		if(newBooks.getId() == 0) {
+			System.out.println("Invalid Id.");
+		}else {
+		System.out.println("Inserted Id is " + newBooks.getId());
+		System.out.println(newBooks.getAuthor());
+		System.out.println("Number of pages: " + newBooks.getNumOfPages());
+		System.out.println("Price is " + newBooks.getPrice() + "$");
+		
+		}
 	}
 }
